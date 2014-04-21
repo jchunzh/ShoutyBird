4e287484da82e194340e539b41002812e09bfc30
James Zhang <jchunzh@gmail.com>
2014-04-21 08:42:02 -0400

    Renamed file containing Position.cs to Vector.cs. Overloaded '+' operator on Vector

---
+++ ShoutyCopter/Vector.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoutyCopter
+{
+    public struct Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector {X = a.X + b.X, Y = a.Y + b.Y};
+        }
+    }
+}
